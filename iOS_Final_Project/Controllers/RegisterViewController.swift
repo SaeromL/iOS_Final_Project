667625d829ae11c0d2582e6f237d49353e9bea1f
Anosha Bari <bariano@sheridancollege.ca>
2024-04-12 01:41:58 -0400

    updated register VC

--- iOS_Final_Project/Controllers/RegisterViewController.swift
+++ iOS_Final_Project/Controllers/RegisterViewController.swift
@@ -5,22 +5,115 @@
 
 
 import UIKit
+import FirebaseAuth
+import FirebaseCoreInternal
+import Firebase
+
 
 class RegisterViewController: UIViewController {
 
-    @IBOutlet weak var tfUsername: UITextField!
+    @IBOutlet weak var tfName: UITextField!
+    @IBOutlet weak var tfEmail: UITextField!
     @IBOutlet weak var tfPassword: UITextField!
     
     @IBOutlet weak var btnRegister: UIButton!
     @IBOutlet weak var btnCancel: UIButton!
     
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setUpElements()
     }
     
+    func setUpElements() {
+    
+        // Hide the error label
+        errorLabel.alpha = 0
+    
+    }
 
+
+    @IBAction func cancelTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func registerTapped(_ sender: Any) {
+        
+        // Validate the fields
+        let error = validateFields()
+        
+        if error != nil {
+            
+            // There's something wrong with the fields, show error message
+            showError(error!)
+        }
+        else {
+            
+            // Create cleaned versions of the data
+            let name = tfName.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let email = tfEmail.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = tfPassword.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            // Create the user
+            Auth.auth().createUser(withEmail: email, password: password) { (result, err) in
+                
+                // Check for errors
+                if err != nil {
+                    
+                    // There was an error creating the user
+                    self.showError("Error creating user")
+                }
+                else {
+                    
+                    // User was created successfully, now store the first name and last name
+                    let db = Firestore.firestore()
+                    
+                    db.collection("users").addDocument(data: ["name":name, "email":email, "uid": result!.user.uid ]) { (error) in
+                        
+                        if error != nil {
+                            // Show error message
+                            self.showError("Error saving user data")
+                        }
+                    }
+                    
+                    // Transition to the home screen
+//                    self.transitionToHome()
+                }
+            }
+        }
+    }
+    
+    func showError(_ message:String) {
+        
+        errorLabel.text = message
+        errorLabel.alpha = 1
+    }
+    
+    
+    func validateFields() -> String? {
+        
+        // Check that all fields are filled in
+        if tfName.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            tfEmail.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            tfPassword.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            
+            return "Please fill in all fields."
+        }
+        
+        // Check if the password is secure
+        let cleanedPassword = tfPassword.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if Utilities.isPasswordValid(cleanedPassword) == false {
+            // Password isn't secure enough
+            return "Password not secure enough."
+        }
+        
+        return nil
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -30,12 +123,5 @@ class RegisterViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
-    @IBAction func cancelTapped(_ sender: Any) {
-    }
-    
-    
-    @IBAction func registerTapped(_ sender: Any) {
-    }
     
 }
