58a9863bfa24dd98ded595009dfbbaa2897c13fa
SaeromL <leesae@sheridancollege.ca>
2024-04-14 17:20:17 -0400

    Edit function added

---
+++ iOS_Final_Project/Controllers/EditViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EditViewController.swift
+//  iOS_Final_Project
+//
+//  Created by Saerom Lee on 2024-04-14.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
